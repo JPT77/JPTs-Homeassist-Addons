82d88412709c06ab6ac04cba651bb33386fb95a3
JPT <j-p-t@gmx.net>
2026-09-05 19:06:10 +0200

    final local installer

--- LoraMqttBridge/PiNode/setup-debug/install.sh
+++ LoraMqttBridge/PiNode/setup-debug/install.sh
@@ -1,3 +1,10 @@
-python .m venv
+#!/bin/sh
+if [[ "$(basename "$PWD")" == "setup-debug" ]]; then
+    echo "Must be called from LoraMqttBridge"
+    exit 1
+fi
+python -m venv
 source bin/activate
 pip install LoRaRF paho-mqtt PyYAML smbus2 spidev gpiod
+
+echo "Start with python -m Lora --config PiNode/config.yaml"
